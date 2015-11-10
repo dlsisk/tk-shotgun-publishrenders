49d6d3f6f38400a8f7cfc583032e1a3b90c9147f
Dave Sisk <dave@janimation.com>
2015-11-10 10:43:05 -0600

    Refactored code to accept a list of templates.
    This commit changes app manifest to require a list of templates (default
    values set are useful for Janimation, your mileage may vary). The app
    now iterates through the list of file types so it can treat different
    files differently.

--- app.py
+++ app.py
@@ -17,23 +17,42 @@ class PublishRenders(Application):
         
                 
         p = {
-            "title": "Publish Rendered Images",
+            "title": "Publish Files",
             "deny_permissions": deny_permissions,
             "deny_platforms": deny_platforms,
             "supports_multiple_selection": True
         }
         
-        self.engine.register_command("publish_renders", self.publish_renders, p)
+        self.engine.register_command("publish_renders", self.publish_files, p)
         
         p = {
-            "title": "Preview Publish Rendered Images",
+            "title": "Preview Publish Files",
             "deny_permissions": deny_permissions,
             "deny_platforms": deny_platforms,
             "supports_multiple_selection": True
         }
         
-        self.engine.register_command("preview_publish", self.preview_publish_renders, p)
-    
+        self.engine.register_command("preview_publish", self.preview_publish_files, p)
+        
+        """ Define templates:
+            self.templates is a list of dictionaries.
+            dictionary entries:
+            self.templates[x]['publish_file']    : template object for filenames we'll be publishing
+            self.templates[x]['publish_name']    : template object for 'name' field in shotgun, to group the files in the loader
+            self.templates[x]['publish_comment'] : template object for 'comment' field (description) in shotgun
+            self.templates[x]['file_type']       : name of published file type for loader hooks
+        """
+        
+        publishes = self.get_setting("publishes")
+        self.templates = []
+        for publish in publishes:
+            self.templates += [
+                                {'publish_file': self.tank.templates[publish['template_publish_file']],
+                                'publish_name': self.tank.templates[publish['template_publish_name']],
+                                'publish_comment': self.tank.templates[publish['template_publish_comment']],
+                                'file_type':publish['published_file_type']}
+            ]
+        
     def _add_plural(self, word, items):
         """
         appends an s if items > 1
@@ -43,7 +62,7 @@ class PublishRenders(Application):
         else:
             return word
 
-    def preview_publish_renders(self, entity_type, entity_ids):
+    def preview_publish_files(self, entity_type, entity_ids):
         
         if len(entity_ids) == 0:
             self.log_info("No entities specified!")
@@ -51,7 +70,9 @@ class PublishRenders(Application):
         
         
         try:
-            paths = self._find_renders(entity_type, entity_ids)
+            paths = []
+            for template in self.templates:
+                paths += self._find_renders(template['publish_file'],entity_type, entity_ids)
             
         
         except tank.TankError, tank_error:
@@ -68,7 +89,7 @@ class PublishRenders(Application):
                 self.log_info("<b>No Publishes would be registered for this item!</b>")
     
             else:
-                self.log_info("<b>Publishing Rendered Images would register %d Published Files on Shotgun:</b>" % len(paths))
+                self.log_info("<b>Publishing Files would register %d Published Files on Shotgun:</b>" % len(paths))
                 self.log_info("")
                 for p in paths:
                     self.log_info(p)
@@ -76,7 +97,7 @@ class PublishRenders(Application):
                 self.log_info("Note that some of these files may be registered on Shotgun already.")
 
 
-    def publish_renders(self, entity_type, entity_ids):
+    def publish_files(self, entity_type, entity_ids):
 
         if len(entity_ids) == 0:
             self.log_info("No entities specified!")
@@ -87,41 +108,40 @@ class PublishRenders(Application):
 
 
         try:
-            paths = self._find_renders(entity_type, entity_ids)
-            
-            entities_processed = len(paths)
 
-            template_sg_publish_name = self.tank.templates[self.get_setting("template_sg_publish_name")]
-            template_sg_publish_comment = self.tank.templates[self.get_setting("template_sg_publish_comment")]
-            template_rendered_image = self.tank.templates[self.get_setting("template_rendered_image")]
-            
-            for path in paths:
-                if len(sgtk.util.find_publish(self.tank,[str(path)])) > 0:
-                    entities_skipped += 1
-                else:
-                    fields = template_rendered_image.get_fields(path)
-                    
-                    # construct kwargs for registration.
-                    try:
-                        comment = template_sg_publish_comment.apply_fields(fields)
-                        publishName = template_sg_publish_name.apply_fields(fields)
-                    except:
-                        raise self.tank.TankError("Failed to apply fields to publish templates!")
+            for template in self.templates:
+                paths = self._find_renders(template['publish_file'], entity_type, entity_ids)
+                
+                entities_processed += len(paths)
+
+                
+                for path in paths:
+                    if len(sgtk.util.find_publish(self.tank,[str(path)])) > 0:
+                        entities_skipped += 1
                     else:
-                        kwargs = {
-                            "tk": self.tank,
-                            "context": self.tank.context_from_path(path),
-                            "comment": comment,
-                            "path": path,
-                            "name": publishName,
-                            "version_number": fields['version'],
-                            "published_file_type":'Rendered Image'
-                        }
-                        # Other potential kwargs could be "thumbnail_path" but it is probably a lot of work to get that added for now.
+                        fields = template['publish_file'].get_fields(path)
+                            
+                        # construct kwargs for registration.
+                        try:
+                            comment = template['publish_comment'].apply_fields(fields)
+                            publishName = template['publish_name'].apply_fields(fields)
+                        except:
+                            raise self.tank.TankError("Failed to apply fields to publish templates!")
+                        else:
+                            kwargs = {
+                                "tk": self.tank,
+                                "context": self.tank.context_from_path(path),
+                                "comment": comment,
+                                "path": path,
+                                "name": publishName,
+                                "version_number": fields['version'],
+                                "published_file_type":template['file_type']
+                            }
+                            # Other potential kwargs could be "thumbnail_path" but it is probably a lot of work to get that added for now.
+                            
+                            # register publish;
+                            publish = sgtk.util.register_publish(**kwargs)
                         
-                        # register publish;
-                        publish = sgtk.util.register_publish(**kwargs)
-                    
 
         except tank.TankError, tank_error:
             # tank errors are errors that are expected and intended for the user
@@ -129,7 +149,7 @@ class PublishRenders(Application):
 
         except Exception, error:
             # other errors are not expected and probably bugs - here it's useful with a callstack.
-            self.log_exception("Error when creating folders!")
+            self.log_exception("Error when registering Published Files! %s" % error)
 
         else:
             # report back to user
@@ -138,9 +158,12 @@ class PublishRenders(Application):
                                                             self._add_plural(entity_type, len(entity_ids)), 
                                                             entities_processed, entities_skipped))
 
-    def _find_renders(self, entity_type, entity_ids):
-
-        template_rendered_image = self.tank.templates[self.get_setting("template_rendered_image")]
+    def _find_renders(self, template, entity_type, entity_ids):
+        """
+        template: sg template object to search for
+        entity_type: string, should be 'Shot' or 'Asset'
+        entity_ids: List of Shotgun IDs
+        """
         
         entity_names = []
         for entity_id in entity_ids:
@@ -149,5 +172,6 @@ class PublishRenders(Application):
 
         paths = []
         for entity_name in entity_names:
-                paths += self.tank.abstract_paths_from_template(template_rendered_image, {entity_type:entity_name})
+                paths += self.tank.abstract_paths_from_template(template, {entity_type:entity_name})
+        paths.sort()
         return paths
